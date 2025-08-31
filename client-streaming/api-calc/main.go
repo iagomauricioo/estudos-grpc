b1e435358f5a2b16acb131d960efc3252496484f
Iago Mauricio <iagomauricio7@gmail.com>
2025-08-31 11:38:39 -0300

    funcionando o envio por streaming do lado do client

--- client-streaming/api-calc/main.go
+++ client-streaming/api-calc/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"client-streaming/src/pb/calc"
+	"fmt"
+	"log"
+	"net"
 
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	calc.CalcServiceServer
+}
+
+func (s *server) Calc(stream calc.CalcService_CalcServer) error {
+	for {
+		input, err := stream.Recv()
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("input: %+v\n", input)
+	}
+}
+
+func main() {
+	log.Println("starting gRPC server")
+
+	lis, err := net.Listen("tcp", ":9090")
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	calc.RegisterCalcServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	log.Println("gRPC server started port 9090")
 }
